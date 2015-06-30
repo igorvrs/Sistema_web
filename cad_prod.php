e45f4baca4e18c70c5eb3c88d8029e9786483bbc
igorvrs <igor.vs19@gmail.com>
2015-06-30 13:26:22 -0300

    validacao

--- cad_prod.php
+++ cad_prod.php
@@ -4,6 +4,9 @@ include('conecta.php');
 $fornec_id = $_POST['id_forn'];
 $nome = $_POST['nome'];
 $selecione = $_POST['selecione'];
+$selecione = iconv('UTF-8', 'ISO-8859-1', $selecione);
+echo($selecione);
+exit();
 //$selecione = utf8_decode($selecione);
 $qtd_estoque = $_POST['qtd_estoque'];
 $valor_unit = $_POST['valor'];
@@ -19,21 +22,22 @@ else
 
 $res = mysql_query("select * from fornecedores where forn_id = '".$fornec_id."'");
 
-while($escrever=mysql_fetch_array($res)){
+if (mysql_affected_rows() == 0){
+   echo '<script> alert ("Fornecedor não encontrado."); 
+   window.location.href="form_produto.html"</script>';
+   
+}
 
-if ($escrever['forn_id'] == '')
-   echo '<script> alert ("Fornecedor nao encontrado.")</script>';
-else{
+while($escrever=mysql_fetch_array($res)){
+   
    if (empty($fornec_id) || empty($nome) || empty($selecione) || empty($qtd_estoque) || empty($valor_unit) || empty($valor_venda) ||
-   empty($valor_desc) || empty($descricao)){
+   empty($valor_desc) || empty($descricao))
        echo '<script> alert ("Dados incomplemtos.") </script>';
 	   
-	} else {
+	 else 
 
  $sql = mysql_query ("INSERT INTO produto (prod_nome, prod_tipo, prod_desc, prod_valorunit, prod_valorvenda, fornec_id, prod_desconto, prod_qtdestoque) values	
   ('".$nome."', '".$nome."', '".$descricao."', '".$valor_unit."', '".$valor_venda."', '".$fornec_id."', '".$valor_desc."', '".$qtd_estoque."')")
-   or die (mysql_error());      
-    }
-}
-}
+   or die (mysql_error());        
+}    
 ?>
